20e38487b538524762bbff223f75d646ed10246b
Stijn Tintel <stijn@linux-ipv6.be>
2025-05-16 20:57:49 +0300

    api: add GET /api/config
    Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,14 +1,19 @@
+use std::sync::Arc;
+
 use axum::Router;
+use config::config_routes;
 use info::info_routes;
 use release::release_routes;
 
 use crate::state::SharedState;
 
+pub mod config;
 pub mod info;
 pub mod release;
 
 pub fn api_routes(state: SharedState) -> Router<()> {
     Router::new()
+        .nest("/config", config_routes(Arc::clone(&state)))
         .nest("/info", info_routes())
         .nest("/release", release_routes(state))
 }
