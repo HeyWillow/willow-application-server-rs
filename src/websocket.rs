5da66cf8f7c4e85d601ec0d9f83b1bbf5f9b8047
Stijn Tintel <stijn@linux-ipv6.be>
2025-05-16 18:07:27 +0300

    websocket: handle different message types
    Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

--- src/websocket.rs
+++ src/websocket.rs
@@ -1,5 +1,8 @@
 use axum::{
-    extract::{WebSocketUpgrade, ws::WebSocket},
+    extract::{
+        WebSocketUpgrade,
+        ws::{Message, WebSocket},
+    },
     http::HeaderMap,
     response::IntoResponse,
 };
@@ -15,7 +18,18 @@ async fn handle_ws(mut ws: WebSocket) {
     tracing::debug!("{ws:#?}");
 
     while let Some(Ok(msg)) = ws.recv().await {
-        tracing::debug!("received WebSocket message: {msg:#?}");
+        tracing::trace!("received WebSocket message: {msg:#?}");
+
+        match msg {
+            Message::Binary(_) => {
+                tracing::error!("binary WebSocket messages not supported");
+            }
+            Message::Close(m) => todo!("close message {m:?}"),
+            Message::Ping(_) | Message::Pong(_) => {}
+            Message::Text(m) => {
+                tracing::debug!("received WebSocket TEXT message: {m:?}");
+            }
+        }
     }
 }
 
