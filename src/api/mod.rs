f4d14a2cc28bedfb73d38c5e3e7de25ba1848148
Stijn Tintel <stijn@linux-ipv6.be>
2025-05-17 00:41:26 +0300

    api: add GET and POST /api/client
    Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,18 +1,21 @@
 use std::sync::Arc;
 
 use axum::Router;
+use client::client_routes;
 use config::config_routes;
 use info::info_routes;
 use release::release_routes;
 
 use crate::state::SharedState;
 
+pub mod client;
 pub mod config;
 pub mod info;
 pub mod release;
 
 pub fn api_routes(state: SharedState) -> Router<()> {
     Router::new()
+        .nest("/client", client_routes(Arc::clone(&state)))
         .nest("/config", config_routes(Arc::clone(&state)))
         .nest("/info", info_routes())
         .nest("/release", release_routes(state))
