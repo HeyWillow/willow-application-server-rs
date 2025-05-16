463b3ecc95c05dafa08b4909ab751639d0f1186a
Stijn Tintel <stijn@linux-ipv6.be>
2025-05-16 18:56:50 +0300

    willow: add goodbye message
    As this contains the same data as the hello message, rename the hello
    message and reuse it.
    
    Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

--- src/willow/messages.rs
+++ src/willow/messages.rs
@@ -3,11 +3,12 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "snake_case")]
 pub enum WillowMsg {
-    Hello(WillowMsgHello),
+    Goodbye(WillowMsgGoodbyeHello),
+    Hello(WillowMsgGoodbyeHello),
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-pub struct WillowMsgHello {
+pub struct WillowMsgGoodbyeHello {
     hostname: String,
     hw_type: String,
     mac_addr: Vec<u8>,
