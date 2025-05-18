eb81a09cef39a4da505dc8bacceb274748d4f052
Stijn Tintel <stijn@linux-ipv6.be>
2025-05-18 11:10:26 +0300

    willow: add custom deserializers for config
    The Python version stores string, int and bool in the same VARCHAR
    column in the database, and converts it back to the original type by
    mapping the database query result to a Pydantic model.
    
    Unfortunately we cannot use sqlx query_as to automagically do the same.
    We therefore need a few custom deserializers.
    
    Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

--- src/willow/config.rs
+++ src/willow/config.rs
@@ -1,4 +1,6 @@
-use serde::{Deserialize, Serialize};
+use std::{fmt::Display, str::FromStr};
+
+use serde::{Deserialize, Deserializer, Serialize};
 
 #[derive(Deserialize, Serialize)]
 #[serde(rename_all = "SCREAMING-KEBAB-CASE")]
@@ -81,47 +83,73 @@ pub enum WillowWakeWord {
 #[allow(clippy::struct_excessive_bools)]
 #[derive(Deserialize, Serialize)]
 pub struct WillowConfig {
+    #[serde(deserialize_with = "deserialize_string_to_bool")]
     aec: bool,
     audio_codec: WillowAudioCodec,
     audio_response_type: WillowAudioResponseType,
+    #[serde(deserialize_with = "deserialize_string_to_bool")]
     bss: bool,
     command_endpoint: WillowCommandEndpoint,
+    #[serde(deserialize_with = "deserialize_string_to_number")]
     display_timeout: u32,
     hass_host: Option<String>,
+    #[serde(
+        default,
+        deserialize_with = "deserialize_optional_string_to_option_number"
+    )]
     hass_port: Option<u16>,
+    #[serde(
+        default,
+        deserialize_with = "deserialize_optional_string_to_option_bool"
+    )]
     hass_tls: Option<bool>,
     hass_token: Option<String>,
+    #[serde(deserialize_with = "deserialize_string_to_number")]
     lcd_brightness: u32,
+    #[serde(deserialize_with = "deserialize_string_to_number")]
     mic_gain: u8,
     mqtt_auth_type: Option<WillowMqttAuthType>,
     mqtt_host: Option<String>,
     mqtt_password: Option<String>,
     mqtt_port: Option<String>,
+    #[serde(
+        default,
+        deserialize_with = "deserialize_optional_string_to_option_bool"
+    )]
     mqtt_tls: Option<bool>,
     mqtt_topic: Option<String>,
     mqtt_username: Option<String>,
+    #[serde(deserialize_with = "deserialize_string_to_bool")]
     multiwake: bool,
     ntp_config: WillowNtpConfig,
     ntp_host: Option<String>,
     openhab_token: Option<String>,
     openhab_url: Option<String>,
+    #[serde(deserialize_with = "deserialize_string_to_number")]
     record_buffer: u8,
     rest_auth_header: Option<String>,
     rest_auth_pass: Option<String>,
     rest_auth_type: Option<WillowRestAuthType>,
     rest_auth_user: Option<String>,
     rest_url: Option<String>,
+    #[serde(deserialize_with = "deserialize_string_to_bool")]
     show_prereleases: bool,
+    #[serde(deserialize_with = "deserialize_string_to_number")]
     speaker_volume: u8,
     speech_rec_mode: Option<WillowSpeechRecMode>,
+    #[serde(deserialize_with = "deserialize_string_to_number")]
     stream_timeout: u8,
     timezone: String,
     timezone_name: String,
+    #[serde(deserialize_with = "deserialize_string_to_number")]
     vad_mode: u8,
+    #[serde(deserialize_with = "deserialize_string_to_number")]
     vad_timeout: u32,
+    #[serde(deserialize_with = "deserialize_string_to_bool")]
     wake_confirmation: bool,
     wake_mode: WillowWakeMode,
     wake_word: WillowWakeWord,
+    #[serde(deserialize_with = "deserialize_string_to_bool")]
     was_mode: bool,
     wis_tts_url: Option<String>,
     wis_tts_url_v2: Option<String>,
@@ -146,4 +174,64 @@ pub struct WillowNvsWifi {
 pub struct WillowNvsConfig {
     pub was: WillowNvsWas,
     pub wifi: WillowNvsWifi,
+}
+
+fn deserialize_optional_string_to_option_bool<'de, D>(
+    deserializer: D,
+) -> Result<Option<bool>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let opt_s = Option::<String>::deserialize(deserializer)?;
+    match opt_s {
+        None => Ok(None),
+        Some(s) => match s.to_lowercase().as_str() {
+            "false" => Ok(Some(false)),
+            "true" => Ok(Some(true)),
+            _ => Err(serde::de::Error::invalid_value(
+                serde::de::Unexpected::Str(&s),
+                &"true or false",
+            )),
+        },
+    }
+}
+
+fn deserialize_optional_string_to_option_number<'de, D, T>(
+    deserializer: D,
+) -> Result<Option<T>, D::Error>
+where
+    D: Deserializer<'de>,
+    T: FromStr,
+    <T as FromStr>::Err: Display,
+{
+    let opt_s = Option::<String>::deserialize(deserializer)?;
+    match opt_s {
+        None => Ok(None),
+        Some(s) => T::from_str(&s).map(Some).map_err(serde::de::Error::custom),
+    }
+}
+
+fn deserialize_string_to_bool<'de, D>(deserializer: D) -> Result<bool, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+    match s.to_lowercase().as_str() {
+        "false" => Ok(false),
+        "true" => Ok(true),
+        _ => Err(serde::de::Error::invalid_value(
+            serde::de::Unexpected::Str(&s),
+            &"true or false",
+        )),
+    }
+}
+
+fn deserialize_string_to_number<'de, D, T>(deserializer: D) -> Result<T, D::Error>
+where
+    D: Deserializer<'de>,
+    T: FromStr,
+    <T as FromStr>::Err: Display,
+{
+    let s = String::deserialize(deserializer)?;
+    T::from_str(&s).map_err(serde::de::Error::custom)
 }
